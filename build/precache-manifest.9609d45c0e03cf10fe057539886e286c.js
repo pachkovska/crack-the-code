6110c445d3ef0593c031381da06df42e1e8af770
pachkovska <pachkovska@gmail.com>
2020-03-27 22:07:43 -0700

    add gitignore

---
+++ build/precache-manifest.9609d45c0e03cf10fe057539886e286c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffd7449e9141f5720b0572562f157b62",
+    "url": "/crack-the-code/index.html"
+  },
+  {
+    "revision": "daa11c75878bea6df4cb",
+    "url": "/crack-the-code/static/css/main.4e02db85.chunk.css"
+  },
+  {
+    "revision": "28e975199319cd07e47c",
+    "url": "/crack-the-code/static/js/2.24b9e622.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/crack-the-code/static/js/2.24b9e622.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "daa11c75878bea6df4cb",
+    "url": "/crack-the-code/static/js/main.6e9c7616.chunk.js"
+  },
+  {
+    "revision": "e8b442ca8225c090bfb0",
+    "url": "/crack-the-code/static/js/runtime-main.0557be40.js"
+  },
+  {
+    "revision": "878b266109b8d0b5adf5d17497470c6d",
+    "url": "/crack-the-code/static/media/logo.878b2661.png"
+  }
+]);
